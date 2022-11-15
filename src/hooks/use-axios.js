330a2de4ce2e81034be8c76015d87fa5f57f3018
Fengbo Liang <itbo@163.com>
2022-11-15 21:49:23 +0800

    style: 微调样式

--- src/hooks/use-axios.js
+++ src/hooks/use-axios.js
@@ -1,9 +1,4 @@
 import { useCallback } from 'react'
 import { postRequest } from '../request'
-import { useAdminInfo } from './use-admin'
 
-export const useAxios = () => {
-  const { admin } = useAdminInfo()
-  console.log('admin', admin?.id)
-  return useCallback(postRequest, [admin?.id])
-}
+export const useAxios = () => useCallback(postRequest, [])
